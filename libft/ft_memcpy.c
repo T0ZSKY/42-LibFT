8e87364a29e7c716dbacb95f94643c829b098de4
Tom Limon <tomlimon@c3r4p4.42perpignan.fr>
2024-11-04 16:20:47 +0100

    first commit

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tomlimon <tomlimon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 14:34:50 by tomlimon          #+#    #+#             */
+/*   Updated: 2024/11/04 15:57:15 by tomlimon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void *memcpy(void *dest, const void *src, size_t n)
+{
+	char	*de;
+	char	*sr;
+	size_t	i;
+
+	i = 0;
+	sr = (char *) src;
+	de = (char *) dest;
+	while(i < n)
+	{
+		de[i] = sr[i];
+		i++;
+	}
+	return dest;	
+}
+/*
+#include <stdio.h>
+
+int main()
+{
+    char src[20] = "Hello, world!";
+    char dest[20];
+
+    printf("Avant memcpy : %s\n", dest);
+
+    memcpy(dest, src, strlen(src) + 1);
+
+    printf("Après memcpy : %s\n", dest);
+
+    return 0;
+}
+*/
---
+++ libft/ft_memmove.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tomlimon <tomlimon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 14:42:06 by tomlimon          #+#    #+#             */
+/*   Updated: 2024/11/04 16:18:51 by tomlimon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void *memmove(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+	char	*de;
+	char	*sr;
+
+	if (!dest && !src)
+		return (NULL);
+	sr = (char *) = src;
+	de = (char *) = dest;
+	i = 0;
+	if (de > sr)
+	{
+		
+	}
+}
