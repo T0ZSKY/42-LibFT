f5fb863d991da73ee9e6681a5e55d9eedda5172d
Tom Limon <tomlimon@c3r4p4.42perpignan.fr>
2024-11-04 18:05:28 +0100

    push

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,23 +6,52 @@
 /*   By: tomlimon <tomlimon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 14:42:06 by tomlimon          #+#    #+#             */
-/*   Updated: 2024/11/04 16:18:51 by tomlimon         ###   ########.fr       */
+/*   Updated: 2024/11/04 16:46:29 by tomlimon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void *memmove(void *dest, const void *src, size_t n)
+#include <string.h>
+
+void	*memmove(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 	char	*de;
 	char	*sr;
 
-	if (!dest && !src)
-		return (NULL);
-	sr = (char *) = src;
-	de = (char *) = dest;
+	sr = (char *) src;
+	de = (char *) dest;
 	i = 0;
 	if (de > sr)
 	{
-		
+		while (n > 0)
+		{
+			n--;
+			de[n] = sr[n];
+		}
 	}
+	else
+	{
+		while (i < n)
+		{
+			de[i] = sr[i];
+			i++;
+		}
+	}
+	return (dest);
 }
+/*
+#include <stdio.h>
+
+int main()
+{
+    char buffer[20] = "Hello, world!";
+
+    printf("Avant memmove : %s\n", buffer);
+
+    memmove(buffer + 7, buffer, 5);
+
+    printf("Après memmove : %s\n", buffer);
+
+    return 0;
+}
+*/
