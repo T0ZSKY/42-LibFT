c993370c5af7e0745ae9093fa0def3aaf7bf69a7
Tom Limon <tomlimon@Macbook-de-Tom.local>
2024-11-08 11:36:04 +0100

    push

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomlimon <tomlimon@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tomlimon <tom.limon@>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 15:24:39 by tomlimon          #+#    #+#             */
-/*   Updated: 2024/11/07 15:47:07 by tomlimon         ###   ########.fr       */
+/*   Updated: 2024/11/07 21:34:04 by tomlimon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,26 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*old;
-	t_list	*new;
+	t_list	*new_list;
+	t_list	*new_elem;
 
-	new = ft_lstnew(f(lst->content));
-	old = new;
+	if (!lst || !f)
+		return (NULL);
+	new_list = ft_lstnew(f(lst->content));
+	if (!new_list)
+		return (NULL);
 	lst = lst->next;
+	new_elem = new_list;
 	while (lst)
 	{
-		new->next = ft_lstnew(f(lst->content));
-		if (!new->next)
+		new_elem->next = ft_lstnew(f(lst->content));
+		if (!new_elem->next)
 		{
-			ft_lstclear(&old, del);
-			return (0);
+			ft_lstclear(&new_list, del);
+			return (NULL);
 		}
-		new = new->next;
+		new_elem = new_elem->next;
 		lst = lst->next;
 	}
-	new->next = NULL;
-	return (old);
+	return (new_list);
 }
